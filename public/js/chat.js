61c4a642e5cf9a2dd012cae7530c20e8431ff9a5
ezemg <ezequielmatiasgr@gmail.com>
2023-06-05 12:15:48 -0300

    Enviar mensajes a sala publica y privada

--- public/js/chat.js
+++ public/js/chat.js
@@ -50,14 +50,12 @@ const conectarSocket = async () => {
     console.log('Sockets offline');
   });
 
-  socket.on('recibir-mensajes', () => {
-    // TODO
-  });
+  socket.on('recibir-mensajes', dibujarMensajes);
 
   socket.on('usuarios-activos', dibujarUsuarios);
 
-  socket.on('mensaje-privado', () => {
-    // TODO
+  socket.on('mensaje-privado', (payload) => {
+    console.log('Privado:', payload);
   });
 };
 
@@ -75,6 +73,33 @@ const dibujarUsuarios = (usuarios = []) => {
 
   ulUsuarios.innerHTML = usersHtml;
 };
+
+const dibujarMensajes = (mensajes = []) => {
+  let mensajesHtml = '';
+
+  mensajes.forEach(({ nombre, mensaje }) => {
+    mensajesHtml += `<li>
+      <p>
+      <span class="text-primary">${nombre}:</span>
+      <span>${mensaje}</span>
+      </p> 
+      </li>`;
+  });
+
+  ulMensajes.innerHTML = mensajesHtml;
+};
+
+txtMensaje.addEventListener('keyup', ({ keyCode }) => {
+  const mensaje = txtMensaje.value;
+  const uid = txtUid.value;
+
+  if (keyCode !== 13) return;
+  if (mensaje.length === 0) return;
+
+  socket.emit('enviar-mensaje', { mensaje, uid });
+
+  txtMensaje.value = '';
+});
 
 const main = async () => {
   await validarJWT();
--- sockets/socketController.js
+++ sockets/socketController.js
@@ -15,11 +15,27 @@ socketController = async (socket = new Socket(), io) => {
   // Agregar el usuario conectado
   chatMensajes.conectarUsuario(usuario);
   io.emit('usuarios-activos', chatMensajes.usuariosArr);
+  socket.emit('recibir-mensajes', chatMensajes.ultimos10);
+
+  // Conectar a sala privada
+  socket.join(usuario.id);
 
   // Limpiar cuando alguien se desconecta
   socket.on('disconnect', () => {
     chatMensajes.desconectarUsuario(usuario.id);
     io.emit('usuarios-activos', chatMensajes.usuariosArr);
+  });
+
+  socket.on('enviar-mensaje', ({ uid, mensaje }) => {
+    if (uid) {
+      socket.to(uid).emit('mensaje-privado', {
+        de: usuario.nombre,
+        mensaje,
+      });
+    } else {
+      chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
+      io.emit('recibir-mensajes', chatMensajes.ultimos10);
+    }
   });
 };
 
